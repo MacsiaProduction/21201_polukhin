3e81b18fc71b5c2b3327254bbd3caca233a944f0
MacsiaProducton <Macsia.Production@gmail.com>
2022-10-25 18:48:29 +0700

    new log system

---
+++ res/log.hpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+enum typelog {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR
+};
+
+extern typelog log_level;
+
+class LOG {
+public:
+    LOG() {}
+    LOG(typelog type) {
+        msglevel = type;
+        operator << ("["+getLabel(type)+"] ");
+    }
+    ~LOG() {
+        if(opened) {
+            std::cout << std::endl;
+        }
+        opened = false;
+    }
+    template<class T>
+    LOG &operator<<(const T &msg) {
+        if(msglevel >= log_level) {
+            std::cout << msg;
+            opened = true;
+        }
+        return *this;
+    }
+private:
+    bool opened = false;
+    typelog msglevel = DEBUG;
+    inline std::string getLabel(typelog type) {
+        std::string label;
+        switch(type) {
+            case DEBUG: label = "\033[1;36mDEBUG\033[0m"; break;
+            case INFO:  label = "\033[1;37mINFO\033[0m"; break;
+            case WARN:  label = "\033[1;33mWARN\033[0m"; break;
+            case ERROR: label = "\033[1;31mERROR\033[0m"; break;
+        }
+        return label;
+    }
+};
