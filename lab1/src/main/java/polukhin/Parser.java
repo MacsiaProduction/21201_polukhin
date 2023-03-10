4a572e04dafacf6804021ed911d1720b0ee22796
Artemiy Sartakov <artfly94@gmail.com>
2023-03-10 13:43:34 +0700

    cr

--- lab1/src/main/java/polukhin/Parser.java
+++ lab1/src/main/java/polukhin/Parser.java
@@ -10,13 +10,26 @@ public class Parser {
      * @param args The command line arguments.
      * @return An instance of the Options class encapsulating the parsed options.
      */
+
+//    private static final org.apache.commons.cli.Options DU_OPTIONS;
+
+//    static {
+//        org.apache.commons.cli.Options options = new org.apache.commons.cli.Options();
+//        options.addOption("depth", true, "maximum depth to search");
+//        options.addOption("L", false, "follow symbolic links");
+//        options.addOption("limit", true, "maximum number of files to display");
+//        options.addOption("h", "help", false, "print help message");
+//    }
+
     public static Options getOptions(String[] args) {
         // Set default values
+        // CR: make constants
         int depth = 10;
         boolean followSymLinks = false;
         int limit = 10;
         Path base_directory = Path.of(".");
         // Define command line options
+        // CR: static field
         org.apache.commons.cli.Options options = new org.apache.commons.cli.Options();
         options.addOption("depth", true, "maximum depth to search");
         options.addOption("L", false, "follow symbolic links");
@@ -49,15 +62,18 @@ public class Parser {
             if (remainingArgs.length != 1) {
                 throw new ParseException("Invalid number of arguments");
             }
+            // CR: validate that it is a directory
             base_directory = Path.of(remainingArgs[0]);
         } catch (ParseException e) {
             System.err.println("Error parsing command line arguments: " + e.getMessage());
             printHelp(options);
+            // CR: move to main
             System.exit(1);
         }
         return new Options(base_directory,depth,followSymLinks,limit);
     }
 
+    // CR: call from main
     private static void printHelp(org.apache.commons.cli.Options options) {
         HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp("java Parser [options] <directory>", options);
--- lab1/src/main/java/polukhin/Types/myDir.java
+++ lab1/src/main/java/polukhin/Types/myDir.java
@@ -9,13 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
-
-import static java.lang.Math.min;
 
 public class myDir extends dirFile {
     private Long size = (long)0;
-    private final ArrayList<dirFile> Children = new ArrayList<>();
+    private final List<dirFile> children = new ArrayList<>();
     public myDir(Path dir, Options options, int mine_depth) {
         super(dir,options, mine_depth);
         if(!getFactoryPredicate().test(dir)) {
@@ -30,7 +29,7 @@ public class myDir extends dirFile {
                 stream.forEach(path -> {
                             dirFile tmp = PathFactory.create(path, options(), mine_depth()+1);
                             size+=tmp.calculateSize();
-                            Children.add(tmp);
+                            children.add(tmp);
                         });
             } catch (IOException e) {
                 throw new RuntimeException("error while calculating size of dir");
@@ -40,15 +39,21 @@ public class myDir extends dirFile {
     }
     @Override
     public void print(Comparator<dirFile> comparator) {
-        Children.sort(comparator);
+        children.sort(comparator);
         System.out.print(" ".repeat(mine_depth())+"/"+file().getFileName()+
                 Converter.convert(calculateSize()) + "\n");
-        for(int i = 1; i <= min(options().limit(),Children.size()); i++) {
-            Children.get(i-1).print(comparator);
+        int nChildren = Math.min(options().limit(), children.size());
+        for(int i = 0; i < nChildren; i++) {
+            children.get(i).print(comparator);
         }
     }
 
     public static Predicate<Path> getFactoryPredicate() {
         return Files::isDirectory;
     }
+
+//    @FunctionalInterface
+//    private interface FilePredicate {
+//        boolean isCompatible(Path path);
+//    }
 }
