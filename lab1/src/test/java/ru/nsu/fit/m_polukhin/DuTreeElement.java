ae9b7535c79020935d9b33f3ad423480af4e8810
MacsiaProduction <Macsia.Production@gmail.com>
2023-05-18 13:46:54 +0700

    cosmetic

--- lab1/src/test/java/ru/nsu/fit/m_polukhin/DuTreeElement.java
+++ lab1/src/test/java/ru/nsu/fit/m_polukhin/DuTreeElement.java
@@ -1,13 +1,9 @@
 package ru.nsu.fit.m_polukhin;
 
-import ru.nsu.fit.m_polukhin.exceptions.ClassLoadException;
-import ru.nsu.fit.m_polukhin.exceptions.FileMissingException;
-import ru.nsu.fit.m_polukhin.exceptions.PathFactoryException;
 import ru.nsu.fit.m_polukhin.modules.DuFileType;
 import ru.nsu.fit.m_polukhin.modules.dir.DirType;
 import ru.nsu.fit.m_polukhin.modules.file.FileType;
 
-import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -15,7 +11,7 @@ import java.util.List;
 
 public record DuTreeElement(Type type, String path, Long size, List<DuTreeElement> children) {
 
-    public static DuFileType tree(FileSystem fs, DuTreeElement root, JduOptions options) throws ClassLoadException, PathFactoryException, FileMissingException, IOException {
+    public static DuFileType tree(FileSystem fs, DuTreeElement root, JduOptions options)  {
         return buildTree(root, fs.getPath(""), options);
     }
 
