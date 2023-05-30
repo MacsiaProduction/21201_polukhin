d3abf431e88d580e873d2fe8fab6c72de97b170e
MacsiaProduction <Macsia.Production@gmail.com>
2023-05-30 13:08:39 +0700

    cosmetic

--- Influence_Game/src/main/java/ru/nsu/fit/nsu/m_polukhin/model/Field.java
+++ Influence_Game/src/main/java/ru/nsu/fit/nsu/m_polukhin/model/Field.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//todo change state of cells only here
 class Field {
     private final HexCell[][] board;
     private final int rows;
